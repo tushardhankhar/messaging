b8409c70735c0993c21eb086dc480bd0391189c4
tushardhankhar <tushardhankhar98@gmail.com>
2026-09-08 23:48:10 +0530

    Phase 1 completed

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const { join } = require("node:path");
+const { Server } = require("socket.io");
 
 const app = express();
 
 const PORT = 5500;
 
+let httpServer = app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+const io = new Server(httpServer);
+
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+io.on("connection", (socket) => {
+  console.log("Connection Established", socket.id);
+
+  socket.on("join", (userName) => {
+    socket.userName = userName;
+  });
+
+  socket.on("Chat", (msg) => {
+    console.log(msg);
+    socket.emit("Chat Message", msg);
+  });
 });
